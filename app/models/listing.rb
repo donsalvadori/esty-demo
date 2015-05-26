c339c5b1f3e70d84463c061529d9ca99178c048b
Chris Salvi <csalvi42@gmail.com>
2015-05-26 08:27:15 -0700

    added missing comma

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :image, :styles => {  :medium => "200x", :thumb => "100x100>" }, 
-                                            :default_url => "/images/:style/missing.png"
+                                            :default_url => "/images/:style/missing.png",
                                             :storage => :dropbox,
                                             :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                                             :path => ":style/:id_:filename"
