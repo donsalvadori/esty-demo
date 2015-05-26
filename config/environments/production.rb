a3fcf390a926533d2dc8224b30c4514cb2f7999d
Chris Salvi <csalvi42@gmail.com>
2015-05-26 08:18:47 -0700

    trying to get production to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #required for devise. Remember to change localhost to actual app host
-  config.action_mailer.default_url_options = { :host => 'etsydemo-cs.herokuapp.com', :protocol => 'https'  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
