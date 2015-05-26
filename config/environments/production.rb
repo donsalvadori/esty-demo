a6a7149cbda25ebe1dab95981e8e6c54309a6da7
Chris Salvi <csalvi42@gmail.com>
2015-05-26 08:15:14 -0700

    changed production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #required for devise. Remember to change localhost to actual app host
-  config.action_mailer.default_url_options = { host: 'https://etsydemo-cs.herokuapp.com', :protocol => 'https'  }
+  config.action_mailer.default_url_options = { :host => 'etsydemo-cs.herokuapp.com', :protocol => 'https'  }
 end
