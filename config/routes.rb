7a80badd0b4e8f94b33c42847f505e0e9858edfd
Chris Salvi <csalvi42@gmail.com>
2015-05-30 16:33:50 -0700

    created order history pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   end
 
   get 'pages/about'
-
   get 'pages/contact'
+  
   get 'seller' => 'listings#seller'
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
+
   root 'listings#index'
 end
